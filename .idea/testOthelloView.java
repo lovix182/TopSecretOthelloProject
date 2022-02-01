d894f00ce0d1a312e0fea0c5f7c38c64fe371b2f
lovix182 <rosin.lovisa@gmail.com>
2022-02-01 14:17:23 +0100

    Create testOthelloView.java

---
+++ .idea/testOthelloView.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+
+import javax.swing.*;
+import java.awt.Color;
+
+public class testOthelloView extends JPanel{
+    public static void main(String[] args) {
+    JFrame f = new JFrame("Othello!");
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setSize(600, 600);
+    f.setLocation(500, 200);
+    
+    Color custom = new Color(4,57,39);
+    JPanel gamePanel = new JPanel();
+    gamePanel.setBackground(custom);
+    gamePanel.setLayout(new GridLayout(10,10,5,5));
+    f.add(gamePanel);
+
+    f.setVisible(true);
+
+
+
+
+    }
+
+    
+}
